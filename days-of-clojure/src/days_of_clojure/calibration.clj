b8a790b6ea86ec54dedc401d56b195f1aa8a81ce
carolina.silva <aanacarolina@gmail.com>
2023-12-04 17:23:30 -0300

    advent of code

---
+++ days-of-clojure/src/days_of_clojure/calibration.clj
@@ -0,0 +1,28 @@
+(ns days-of-clojure.calibration
+  (:require [clojure.string :as str]))
+
+(defn atoi [string]
+  ( str/replace string #"\D" ""))
+
+(defn str->two-digits [digit-str]
+ (str (subs digit-str 0 1) (subs digit-str (dec (count digit-str)))))
+
+(defn calibration
+  [input]
+  (reduce + (map parse-long (map str->two-digits (map atoi (str/split-lines (slurp input)))))))
+
+;ingest input
+;read each line separetedly , vector of strings
+;identify numbers
+;identify first and last numbers
+;if I only have one number repeat it once 
+;;if more than 2 digits consider first last
+;return the 2-digit final number
+;create a collection of all input 2-digit numbers
+;reduce to a final sum 
+
+(comment
+  (calibration "full_input.txt")
+  (atoi "1abc2")
+  (str->two-digits "38")
+  )
---
+++ days-of-clojure/test/days_of_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns days-of-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [days-of-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
