257c8b47e11b1562c90ae5bce465582fad5eb373
carolina.silva <aanacarolina@gmail.com>
2023-12-06 10:28:33 -0300

    mixed dictionary

--- src/advent_of_code/calibration_p2.clj
+++ src/advent_of_code/calibration_p2.clj
@@ -3,10 +3,20 @@
 
 
 
-(def nbr {"eno" "1" "owt" "2" "eerht" "3" "ruof" "4" "evif" "5" "xis" "6" "neves" "7" "thgie" "8" "enin" "9"})
-
+(def nbr {"one" "on1e" 
+          "two" "t2o"
+          "eightwo" "e8t2o"   
+          "three" "t3e" 
+          "four" "f4r" 
+          "five" "f5e" 
+          "six" "s6x" 
+          "seven" "s7n" 
+          "eight" "e8t" 
+          "ight" "eh8t" 
+          "nine" "n9e"})
+;oneight
 (defn str->number [string]
-  (str/replace string #"eno|owt|eerht|ruof|evif|xis|neves|thgie|enin"  #(str (get nbr %))))
+  (str/replace string #"one|two|eightwo|three|four|five|six|seven|eight|ight|nine"  #(str (get nbr %))))
 
 (defn atoi [string]
   (str/replace string #"\D" ""))
@@ -16,27 +26,27 @@
 
 (defn calibration-two
   [input]
-  (reduce + 
-          (map parse-long 
-               (map str->two-digits 
-                    (map atoi 
-                        (map #(apply str (str/reverse %))  
-                             (map str->number 
-                                  (map #(apply str (str/reverse %)) (str/split-lines (slurp input))))))))))
+  (->>
+   (slurp input)
+   (str/split-lines)
+   (map str->number)
+   (map atoi)
+   (map str->two-digits)
+   (map parse-long)
+   (reduce +)))
 
 
- 
+
 ;find spelled number in strings
 ;change for digit value 
-
+;ansers tried: 54853 54913 54871
 (comment
   (calibration-two "full_input.txt")
   (calibration-two "input.txt")
   (apply str (reverse "2ninexblcgmhxxceightwop"))
-  (str->number "powthgiecxxhmgclbxenin2")
-  (apply str (reverse "p2hgiecxxhmgclbx92"))
-  (atoi "29xblcgmhxxceigh2p")
-  (str->two-digits "292")
-  (parse-long "22")
+  (str->number "7bzcnthreejdh7oneightj")
+  (atoi "7bzcnt3ejdh7on1eeh8tj")
+    ;;=> ("2" "4" 1 8 9 8 4)
+  (str->two-digits "2418984")
+  (parse-long "24"))
 
-  )
