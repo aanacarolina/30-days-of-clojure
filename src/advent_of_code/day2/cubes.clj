3cb0411b12f4a5b676f910028582aff364bc072b
carolina.silva <aanacarolina@gmail.com>
2023-12-07 01:01:22 -0300

    game id 2

--- src/advent_of_code/day2/cubes.clj
+++ src/advent_of_code/day2/cubes.clj
@@ -1,38 +1,47 @@
 (ns advent-of-code.day2.cubes
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]))
 
 (def max-cubes {"red" 12 "green" 12 "blue" 14})
 
 (defn input->data [input]
   (str/split-lines (slurp input)))
 
-(defn get-game-id [game-info]
+#_(defn get-game-id [game-info]
   (last (str/split (first (str/split game-info #":")) #" ")))
 
 (defn games-results [game-info]
-  (map #(str/split % #",") (str/split (last (str/split game-info #":")) #";")))
+  (into [] 
+        (map 
+         #(str/split % #":|,") 
+         (str/split game-info #";"))))
+
+(defn game-id [game-results]
+  (map first (map first game-results)))
 
 (defn results->map [game-results]
-  (flatten (map  #(re-seq #"[a-z]+|[0-9]+" %) game-results))
-  )
+  (map (apply hash-map (flatten (map  #(re-seq #"[a-z]+|[0-9]+" %) game-results)))))
 
 (defn eligible-games [])
 
 (defn possible-sum [input]
   (->> input
-       input->data 
-       (map get-game-id)))
+       input->data
+       (map games-results) 
+       ))
+
 
 (comment
   (possible-sum "day2_exinput.txt")
   (possible-sum "day2_input.txt")
   (input->data "day2_input.txt")
-  (input->data "day2_exinput.txt")
-  (get-game-id "Game 50: 10 red, 3 blue, 6 green; 1 blue, 5 red, 3 green; 6 blue, 11 red, 12 green; 10 green")
-  (games-results "Game 50: 10 red, 3 blue, 6 green; 1 blue, 5 red, 3 green; 6 blue, 11 red, 12 green; 10 green")
-  ;=> ([" 10 red" " 3 blue" " 6 green"] [" 1 blue" " 5 red" " 3 green"] [" 6 blue" " 11 red" " 12 green"] [" 10 green"])
-  (results->map [" 10 red" " 3 blue" " 6 green"])
-  ;=> ("10" "red" "3" "blue" "6" "green")
+  (input->data "day2_exinput.txt") 
+  
+  (map first (map first (map games-results ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+                                            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+                                            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+                                            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+                                            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"])))
   )
 
 
