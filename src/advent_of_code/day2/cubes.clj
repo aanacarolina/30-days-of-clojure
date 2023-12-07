d590c29f3efe4f4cdb98dc1bf7ddedf2ad4259d1
carolina.silva <aanacarolina@gmail.com>
2023-12-07 01:38:51 -0300

    its late

--- src/advent_of_code/day2/cubes.clj
+++ src/advent_of_code/day2/cubes.clj
@@ -8,40 +8,47 @@
   (str/split-lines (slurp input)))
 
 #_(defn get-game-id [game-info]
-  (last (str/split (first (str/split game-info #":")) #" ")))
+    (last (str/split (first (str/split game-info #":")) #" ")))
 
 (defn games-results [game-info]
-  (into [] 
-        (map 
-         #(str/split % #":|,") 
+  (into []
+        (map
+         #(str/split % #":|,")
          (str/split game-info #";"))))
 
-(defn game-id [game-results]
-  (map first (map first game-results)))
 
-(defn results->map [game-results]
+(defn eligible-games [game-results]
+  (map second (map first game-results)))
+
+(defn game-id [eligible-game-results]
+  (map first (map first eligible-game-results)))
+
+#_(defn results->map [game-results]
   (map (apply hash-map (flatten (map  #(re-seq #"[a-z]+|[0-9]+" %) game-results)))))
 
-(defn eligible-games [])
+(defn split-on-space [s]
+  (str/split (str/triml s) #"\s+"))
+
+(defn results->map [games-results]
+  (map split-on-space games-results))
 
 (defn possible-sum [input]
   (->> input
        input->data
-       (map games-results) 
-       ))
+       (map games-results)
+       results->map))
 
 
 (comment
   (possible-sum "day2_exinput.txt")
   (possible-sum "day2_input.txt")
   (input->data "day2_input.txt")
-  (input->data "day2_exinput.txt") 
-  
-  (map first (map first (map games-results ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-                                            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
-                                            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-                                            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
-                                            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"])))
+  (input->data "day2_exinput.txt")
+  (eligible-games (map games-results (input->data "day2_exinput.txt")))
+  (map first (map first (map games-results (input->data "day2_exinput.txt"))))
+  (map games-results (input->data "day2_exinput.txt"))
+  (results->map ["Game 1" " 3 blue" " 4 red"])
+  ;=> (["Game" "1"] ["3" "blue"] ["4" "red"])
   )
 
 
