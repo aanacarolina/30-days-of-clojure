c816afb838b154606a0d9dc5cb7e68eba9f747cf
carolina.silva <aanacarolina@gmail.com>
2023-12-20 16:30:43 -0300

    working without adjency true

--- src/advent_of_code/day3/gear_ratios.clj
+++ src/advent_of_code/day3/gear_ratios.clj
@@ -8,21 +8,21 @@
 
 ;criar matriz/map
 (def data-map '({:x 0, :y 0, :value "467", :type :digit}
-               {:x 5, :y 0, :value "114", :type :digit}
-               {:x 3, :y 1, :value "*", :type :symbol}
-               {:x 2, :y 2, :value "35", :type :digit}
-               {:x 6, :y 2, :value "633", :type :digit}
-               {:x 6, :y 3, :value "#", :type :symbol}
-               {:x 0, :y 4, :value "617", :type :digit}
-               {:x 3, :y 4, :value "*", :type :symbol}
-               {:x 5, :y 5, :value "+", :type :symbol}
-               {:x 7, :y 5, :value "58", :type :digit}
-               {:x 2, :y 6, :value "592", :type :digit}
-               {:x 6, :y 7, :value "755", :type :digit}
-               {:x 3, :y 8, :value "$", :type :symbol}
-               {:x 5, :y 8, :value "*", :type :symbol}
-               {:x 1, :y 9, :value "664", :type :digit}
-               {:x 5, :y 9, :value "598", :type :digit}))
+                {:x 5, :y 0, :value "114", :type :digit}
+                {:x 3, :y 1, :value "*", :type :symbol}
+                {:x 2, :y 2, :value "35", :type :digit}
+                {:x 6, :y 2, :value "633", :type :digit}
+                {:x 6, :y 3, :value "#", :type :symbol}
+                {:x 0, :y 4, :value "617", :type :digit}
+                {:x 3, :y 4, :value "*", :type :symbol}
+                {:x 5, :y 5, :value "+", :type :symbol}
+                {:x 7, :y 5, :value "58", :type :digit}
+                {:x 2, :y 6, :value "592", :type :digit}
+                {:x 6, :y 7, :value "755", :type :digit}
+                {:x 3, :y 8, :value "$", :type :symbol}
+                {:x 5, :y 8, :value "*", :type :symbol}
+                {:x 1, :y 9, :value "664", :type :digit}
+                {:x 5, :y 9, :value "598", :type :digit}))
 
 #_'([{:x 0, :y 0, :value 467 :type :number}
      {:x 3, :y 0, :value 114 :type :number}]
@@ -78,8 +78,9 @@
          (conj result
                {:x x
                 :y y
-                :value v
-                :type :digit})
+                :value (parse-long v)
+                :width (count v)
+                :type :part})
          (+ x (count v))))
       ;checks if symbol (actually anything other than eol, digit or dot))
       :else
@@ -87,46 +88,48 @@
        (conj result {:x x :y y :value (subs s x (inc x)) :type :symbol})
        (inc x)))))
 
-(defn digits-map [data-map]
-  (filter #(= (:type %) :digit) data-map))
+(defn parts-list [data-list]
+  (filter #(= (:type %) :part) data-list))
 
-(defn symbols-map [data-map]
-  (filter #(= (:type %) :symbol) data-map))
+(defn symbols-list [data-list]
+  (filter #(= (:type %) :symbol) data-list))
 
 (defn digit [digit-map]
   (:value digit-map))
 
-(defn digits-width [digit]
+(defn digit-width [digit]
   (count digit))
 
-(defn is-adjacent? [data])
+(defn is-valid-part? [symbols part]
+  true)
 
 (defn process-gears [input]
-  (->> (input->data input)
-       (map-indexed parse-data)
-       (reduce concat)
-       
-       ))
+  (let [nodes (->> (input->data input)
+                   (map-indexed parse-data)
+                   (reduce concat))
+        parts (parts-list nodes)
+        symbols (symbols-list nodes)]
+    (->> parts
+         (filter #(is-valid-part? symbols %))
+         (map :value)
+         (reduce + ))
+         ))
 
 (comment
 
   (process-gears "day3_exinput.txt")
-
   (process-gears "day3_input.txt")
-  
-  (digits-map data-map)
-  (symbols-map data-map)
 
-  (digits-width (digit {:x 1, :y 9, :value "664", :type :digit}))
-    
+  (parts-list data-map)
+  (symbols-list data-map)
+
+  (digit-width (digit {:x 1, :y 9, :value "664", :type :digit}))
+
   (-> "467.+..114..."
       (.charAt 5)
       Character/isDigit)
    ;Same thing
-  (number? (parse-long (subs "467.+..114..." 5 (inc 5))))
-
-
-  )
+  (number? (parse-long (subs "467.+..114..." 5 (inc 5)))))
 
 
 
