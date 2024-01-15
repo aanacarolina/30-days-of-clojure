65d510d52b58a06d5c9155fb49d033efcdc3d726
carolina.silva <aanacarolina@gmail.com>
2024-01-15 19:25:42 -0300

    parsed-data ok - conver-map WIP

--- src/yr2023/day5/seeds.clj
+++ src/yr2023/day5/seeds.clj
@@ -1,106 +1,98 @@
 (ns yr2023.day5.seeds
-   (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
 
- (defn input->data [input]
-   (str/split-lines (slurp input)))
+(defn input->data [input]
+  (str/split-lines (slurp input)))
 
- (defn parse-data [data]
-   "convert string to data structure"
-   data)
- ;blank line
- ;name of maps
- ;sequences of nbrs
- 
- ;EOF = nil (end condition for parsing)
-
- (defn convert-maps [parsed-data]
-   "converts numbers from a source category into numbers in a destination category"
-   parsed-data)
-
-#_(desired parsed data format
- {:seeds [79 14 55 13]
- :categories-conversion-maps 
-  {:seed-to-soil [[50 98 2] [52 50 48]]
-  :soil-to-fertilizer [[0 15 37][37 52 2][39 0 15]]
-  :fertilizer-to-water [[][][][]]
-  }
-  })
- 
-;each map has same [index 0 = destination-start / index 1 = source-start / index 2 = range] 
-;[50 98 2] range 2 
-;50 = 98 
-;51 = 99
-
-;[52 50 48] - range 48
-;52 = 50
-;100 = 98
-;but map can have different ammount of conversion instructions
- 
- ;conversion formula: 
- ;location is the result of all conversions
+(defn parse-data [data]
+  "convert string to data structure"
+  (let [seeds (first (map #(re-seq  #"\d+" %) data))
+        int-maps (->> (rest data)
+                      (map #(re-seq #"\d+" %))
+                      (remove nil?)
+                      (remove empty?)
+                      (map #(->> % (map parse-long) vec)))]
+    {:seeds (vec (map parse-long seeds))
+     :maps int-maps}))
 
 
-(defn seeds [input]
-  (->> (input->data input)))
-
-(comment
-  (seeds "inputs/day5_exinput.txt")
-
+;each map vector has [index 0 = dst / index 1 = src / index 2 = rng]
 ;(some #(= seed %) (or (range  dst (+ dst rng)) (range src (+ src rng)))) 
 ;call again, but seed value will now be
 ;if nil/false = current value of seed
-;if true (+ seed (dst - src))
-;if eol = return location =  last seed value
-;  
+;if true seed = (+ seed (dst - src))
+;if end of array = last seed value
+;save location
+;return seq w/ all locations
 
-  (some #(= seed %) (or (range  dst (+ dst rng)) (range src (+ src rng))))
+(defn convert-maps [seed maps]
+  "converts numbers from a source category into numbers in a destination category"
+  (loop [map (first maps)]
+    (if (seq? map)
+      (let [dst (get map 0)
+            src (get map 1)
+            rng (get map 2)]
+        (if
+         (= true (some #(= seed %) (or (range  dst (+ dst rng)) (range src (+ src rng)))))
+          (+ seed (dst - src))
+          seed))
+      (recur (rest maps)))))
 
-  ;each map has same [index 0 = dst / index 1 = src / index 2 = rng]
 
-  ;1 seed
-  79
-  ;2 seed-to-soil
-  (some #(= 79 %) (or (range  50 (+ 50 2)) (range 98 (+ 98 2))))
-  79
-  (some #(= 79 %) (or (range  52 (+ 52 48)) (range 50 (+ 50 48))))
-  (+ 79 (- 52 50))
+(defn smallest-location [input]
+  (->> (input->data input)
+       parse-data
+       #_convert-maps
+       #_(min)))
 
-  ;3 soil-to-fertilizer
-  (some #(= 81 %) (or (range  0 (+ 0 37)) (range 15 (+ 15 37))))
-  (some #(= 81 %) (or (range  37 (+ 37 2)) (range 52 (+ 52 2))))
-  (some #(= 81 %) (or (range  39 (+ 39 15)) (range 0 (+ 0 15))))
-
-  ;4 fertilizer-to-water
-  (some #(= 81 %) (or (range  49 (+ 49 8)) (range 53 (+ 53 8))))
-  (some #(= 81 %) (or (range  0 (+ 0 42)) (range 11 (+ 11 42))))
-  (some #(= 81 %) (or (range  42 (+ 42 7)) (range 0 (+ 0 7))))
-  (some #(= 81 %) (or (range  57 (+ 57 4)) (range 7 (+ 7 4))))
-
-  ;5 water-to-light
-  (some #(= 81 %) (or (range  88 (+ 88 7)) (range 18 (+ 18 7))))
-  (some #(= 81 %) (or (range  18 (+ 18 70)) (range 25 (+ 25 70))))
-  (+ 81 (- 18 25))
-
-  ;6 light-to-temperature
-  (some #(= 74 %) (or (range  45 (+ 45 23)) (range 77 (+ 77 23))))
-  (some #(= 74 %) (or (range  81 (+ 81 19)) (range 45 (+ 45 19))))
-  (some #(= 74 %) (or (range  68 (+ 68 13)) (range 64 (+ 64 13))))
-  (+ 74 (- 68 64))
-
-  ;7 temperature-to-humidity
-  (some #(= 78 %) (or (range  0 (+ 0 1)) (range 69 (+ 69 1))))
-  (some #(= 78 %) (or (range  1 (+ 1 69)) (range 0 (+ 0 69))))
-
-  ;8 humidity-to-location
-  (some #(= 78 %) (or (range  60 (+ 60 37)) (range 56 (+ 56 37))))
-  (+ 78 (- 60 56))
-  (some #(= 82 %) (or (range  56 (+ 56 4)) (range 93 (+ 93 4))))
-  (+ 82 (- 56 93))
-  ;location = 82  
-  
+(comment
+  (smallest-location "inputs/day5_exinput.txt")
+  (convert-maps 79 '([50 98 2]
+                     [52 50 48]
+                     [0 15 37]
+                     [37 52 2]
+                     [39 0 15]
+                     [49 53 8]
+                     [0 11 42]
+                     [42 0 7]
+                     [57 7 4]
+                     [88 18 7]
+                     [18 25 70]
+                     [45 77 23]
+                     [81 45 19]
+                     [68 64 13]
+                     [0 69 1]
+                     [1 0 69]
+                     [60 56 37]
+                     [56 93 4]))
   )
-#_(destination range start
-               source range start
-               range length)
 
+
+#_(desired parsed data format
+           {:seeds [79 14 55 13]
+            :categories-conversion-maps
+            {:seed-to-soil [[50 98 2] [52 50 48]]
+             :soil-to-fertilizer [[0 15 37] [37 52 2] [39 0 15]]
+             :fertilizer-to-water [[] [] [] []]}}
+           decided to go
+           {:seeds [79 14 55 13],
+            :maps
+            ([50 98 2]
+             [52 50 48]
+             [0 15 37]
+             [37 52 2]
+             [39 0 15]
+             [49 53 8]
+             [0 11 42]
+             [42 0 7]
+             [57 7 4]
+             [88 18 7]
+             [18 25 70]
+             [45 77 23]
+             [81 45 19]
+             [68 64 13]
+             [0 69 1]
+             [1 0 69]
+             [60 56 37]
+             [56 93 4])})
